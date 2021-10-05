d9769654bf7de1064b94fa1d9be97b33f19e3cfb
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-05 14:51:28 +0530

    v1

---
+++ app.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const session = require("express-session");
+const mysql = require("mysql2");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: process.env.SECRECT,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // 86400000 1 day
+    }
+}));
+
+// database connection
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.connect(function(err){
+    if(err){
+        throw err;
+    } else {
+        console.log("connected to database");
+    }
+});
+
+app.get("/", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+});
